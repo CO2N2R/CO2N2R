f8da4e3ba127312a084db38d4b523115ff2c483f
Connor Barton <connor.barton1152@gmail.com>
2025-06-02 15:49:16 -0500

    20250602

--- practice.py
+++ practice.py
@@ -1,25 +1,26 @@
 # import dependencies 
-import numpy, pandas    # pip install _
-import random
+import numpy, pandas, random, argparse    
+# pip install _
 
-#
 Date: str = "2025 Jan 29"
 number: float = 3.141592
 integer: int = 10
 
-def generator(num):
+def generator(num: int):
     for _ in range(3):
-        yield random.randint(num, integer)
+        yield random.randint(num: int, integer: int)
 
 #def user_input():
  #   user_input = input("Please enter integer:")
 
 def sets():
     set_of_fruits ={"apple", "banana", "orange"}
-    assert isinstance(set_of_fruits)
+    assert isinstance(set_of_fruits, tuple)
 
 def tuples():
     tuple_of_veggies = ("carrot","cauliflower","lettuce")
+    
+    print(tuple_of_veggies.count)
 
 function()
 # user_input()
@@ -28,7 +29,7 @@ print(generator(input))
 
 class ComplexNumberWithConstructor:
         """Example of the class with constructor"""
-        def __init__(self, real_part, imaginary_part):
+        def __init__(self, real_part: float, imaginary_part: float):
             self.real = real_part
             self.imaginary = imaginary_part
 
